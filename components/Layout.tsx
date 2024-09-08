d42c9f67b3451b4b8f905c5cf4a360177a2aa8e4
Rodrigo De La Torre <cognitiveds.ai@gmail.com>
2024-09-07 21:35:38 -0600

    Add Final

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from 'react';
-import Footer from './Footer';
 import Header from './Header';
 
 interface LayoutProps {
@@ -11,7 +10,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-brand-purple-dark via-brand-purple to-brand-bg">
       <Header />
       {children}
-      <Footer />
     </div>
   );
 };
